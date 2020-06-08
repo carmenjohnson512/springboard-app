1ffab4f406585ae61f7f9a78fe6bd3e3c87bacd9
Mark McDonald <markwilliammcdonald@gmail.com>
2020-06-08 01:18:54 -0500

    played with the Ticketmaster API. Ran into some console problems, so we will need to discuss as a group. reverted some changes I made back to as close to the original js as I could, so there are not many changes for this commit

--- assets/springboard.js
+++ assets/springboard.js
@@ -840,26 +840,26 @@ $(document).ready(function () {
         }
       ]
   };
-  
+
   let queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&start=0&count=100&cuisines=1%2C151%2C3%2C193&sort=rating&order=desc";
   const APIKey = "cd932dfc82bc08b58c79cefff1fc925a";
   const APIKey2 = "1092a507c481907491fcd43ea457fbd9";
 
-//   $.ajax({
-//     dataType: "json",
-//     url: queryURL,
-//     method: "GET",
-//     crossDomain: true,
-//     async: true,
-//     headers: {
-//       "user-key": APIKey
-//     }
-//   }).then(function (data) {
-//     console.log(data)
-//   });
- 
+  //   $.ajax({
+  //     dataType: "json",
+  //     url: queryURL,
+  //     method: "GET",
+  //     crossDomain: true,
+  //     async: true,
+  //     headers: {
+  //       "user-key": APIKey
+  //     }
+  //   }).then(function (data) {
+  //     console.log(data)
+  //   });
+
   //function to take city name input and populate city name & city-based restaurant and event recommendations 
-  $("#searchBtn").on("click", function currentCity () {
+  $("#searchBtn").on("click", function currentCity() {
     let searchInput = $("#search-bar").val();
     let citiesURL = "https://developers.zomato.com/api/v2.1/cities?q=" + searchInput;
     // console.log("this is what we typed", searchInput);
@@ -877,8 +877,8 @@ $(document).ready(function () {
       let cityName = data.location_suggestions[0].name;
       console.log("city ID from API? ", cityID);
       $("#cityTitle").text("Welcome to " + cityName);
-    //   console.log("is city name working?", cityName)
-      
+      //   console.log("is city name working?", cityName)
+
       restaurantRecs(cityID);
     });
   });
@@ -892,36 +892,36 @@ $(document).ready(function () {
     $(".dropdown-content").append(dropdownItem);
     console.log("appending cuisines?")
   };
-  
+
   //function to populate restaurant recommendations by cityID
   function restaurantRecs(cityId) {
     for (let i = 0; i < cuisineOptions.cuisines.length; i++) {
-   
-        let cuisineID = cuisineOptions.cuisines[i].cuisine.cuisine_id;
-        console.log("cuisineID?")
-        let restaurantsURL =
+
+      let cuisineID = cuisineOptions.cuisines[i].cuisine.cuisine_id;
+      console.log("cuisineID?")
+      let restaurantsURL =
         "https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&start=0&count=100&cuisines=" + cuisineID + "&sort=rating&order=desc";
 
-        $.ajax({
+      $.ajax({
         dataType: "json",
         url: restaurantsURL,
         method: "GET",
         crossDomain: true,
         async: true,
         headers: {
-            "user-key": APIKey2,
+          "user-key": APIKey2,
         },
-        }).then(function (data) {
-            console.log("recommendation data!!!!", data);
-            
-        });
+      }).then(function (data) {
+        console.log("recommendation data!!!!", data);
+
+      });
     };
   };
 
   $.ajax({
     type: "GET",
     url:
-      "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Zm5ycfcSybGtmXIn4dDXF1fCqr8xTo2A",
+      "https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Zm5ycfcSybGtmXIn4dDXF1fCqr8xTo2A&locale=*&city=Austin&countryCode=US",
     async: true,
     dataType: "json",
     success: function (json) {
